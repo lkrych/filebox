74d7e96c4bde8dbe6f0dd21bf99f38b442c89581
Leland Krych <Leland.Krych@gmail.com>
2017-07-31 15:21:04 -0700

    Create form-upload component in React

---
+++ client/src/components/file_uploader.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+class FileUploader extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: ''
+    };
+  }
+
+  updateName(event){
+    this.setState({name: event.target.value});
+  }
+
+  uploadFile(event, { file }){
+    const data = new FormData();
+    event.preventDefault();
+    data.append('file', event.target.file.files[0]);
+    data.append('originalFileName', event.target.file.files[0].name);
+    data.append('givenFileName', event.target.fileName.value);
+    data.append('fileDescription', event.target.fileDescription.value);
+    console.log('blah blah blah');
+    console.log('Inside uploadFile: ', data);
+    debugger;
+  }
+
+  render(){
+    return(
+      <div>
+      <h1> JSX is rendering </h1>
+        <form
+        onSubmit={this.uploadFile}
+        className="form-uploader"
+        encType="multipart/form-data">
+          <label htmlFor="fileName">File Name:
+            <input type="text" name="fileName"
+            value={this.state.name}
+            onChange={this.updateName.bind(this)}/>
+          </label>
+          <label htmlFor="fileDescription">File Description:
+            <textarea
+            name="fileDescription"
+            id="textarea"
+            rows="8" cols="60">
+            </textarea>
+          </label>
+          <input type="file" name="file" />
+          <input type="submit" name="submit" value="Upload file" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default FileUploader;
