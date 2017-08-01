960a3e18b1ddb8f6f5d554e74e08e3670381ddb4
Leland Krych <Leland.Krych@gmail.com>
2017-07-31 17:42:37 -0700

    Add axios package and change reference to actionz

--- client/src/reducers/files_reducer.js
+++ client/src/reducers/files_reducer.js
@@ -1,12 +1,12 @@
 import _ from 'lodash';
 
-import { FETCH_FILES, UPLOAD_FILE } from '../actions';
+import { FETCH_FILES, UPLOAD_FILE } from '../actionz';
 
 export default function(state = {}, action) {
   switch (action.type){
     case UPLOAD_FILE:
       return { ...state, [ action.payload.data.id]: action.payload.data };
-    case FETCH_POSTS:
+    case FETCH_FILES:
       return _.mapKeys(action.payload.data, 'id'); //soooo useful
     default:
       return state;
